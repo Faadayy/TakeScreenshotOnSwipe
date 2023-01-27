18056a42d21d6ff224f949a9fe2c9289af15dafb
Faadayy <Fahadshahzeb1@gmail.com>
2023-01-27 17:54:59 +0500

    Task Completed

--- App.js
+++ App.js
@@ -1,20 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React, { useState } from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  LogBox,
+} from 'react-native';
+import * as MediaLibrary from 'expo-media-library';
+
+// import CaptureScreen
+import { captureScreen } from 'react-native-view-shot';
+import GestureRecognizer from 'react-native-swipe-detect';
+LogBox.ignoreAllLogs()
+const App = () => {
+  const [imageURI, setImageURI] = useState("");
+  const [savedImagePath, setSavedImagePath] = useState('');
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+
+  if (status === null) {
+    requestPermission();
+  }
+
+  const takeScreenShot = async () => {
+    captureScreen({
+      format: 'jpg',
+      quality: 0.8,
+    }).then(
+      (uri) => {
+        setSavedImagePath(uri);
+        setImageURI(uri);
+        const r = MediaLibrary.saveToLibraryAsync(uri);
+        if (uri) {
+          alert("Screenshot Saved!");
+        }
+      },
+      (error) => console.error('Oops, Something Went Wrong', error),
+    );
+  };
+
+  const config = {
+    velocityThreshold: 0.3,
+    directionalOffsetThreshold: 80,
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureRecognizer
+      onSwipeUp={takeScreenShot}
+      onSwipeDown={takeScreenShot}
+      onSwipeLeft={takeScreenShot}
+      onSwipeRight={takeScreenShot}
+      config={config}
+      style={{
+        flex: 1,
+      }}
+    >
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.container}>
+
+          <Image
+            source={{ uri: imageURI }}
+            style={{
+              width: 200,
+              height: 300,
+              resizeMode: 'contain',
+              marginTop: 5
+            }}
+          />
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            onPress={takeScreenShot}>
+            <Text style={{ color: 'white' }}>
+              Take Screenshot
+            </Text>
+          </TouchableOpacity>
+          <Text >
+            {
+              savedImagePath ?
+                `Saved Image Path\n ${savedImagePath}` : ''
+            }
+          </Text>
+        </View>
+      </SafeAreaView>
+    </GestureRecognizer>
+
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
+    padding: 10,
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50
   },
+  buttonStyle: {
+    backgroundColor: 'black',
+    padding: 5,
+  }
 });
